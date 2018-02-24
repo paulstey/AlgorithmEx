c0403e4a4eb26cf9cd41ac2cbce4e5916f926178
paulstey <paulstey@gmail.com>
2018-02-24 16:53:38 -0500

    minor syntactic changes

--- src/fibonacci.c
+++ src/fibonacci.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 
 int fib(int n) {
-    int a = 1, b = 1;
-    int tmp, i;
+    int a = 1;
+    int b = 1;
+    int tmp;
     
-    for (i = 2; i < n; i++) {
+    for (int i = 2; i < n; i++) {
         tmp = a;
         a = b;
         b = b + tmp;	
@@ -17,7 +18,6 @@ int fib(int n) {
 int main(int argc, char* argv[]) {
     int n = atoi(argv[1]);
  
-
     int res = fib(n);
     printf("%d \n", res);
 }
